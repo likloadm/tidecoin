9432af26dd1a46ef7679c7a777a4902b58a84f07
Yar Sawyer <idquantum@gmail.com>
2022-03-02 18:09:56 +0300

    seeds, message size

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -120,8 +120,10 @@ public:
         // This is fine at runtime as we'll fall back to using them as a oneshot if they don't support the
         // service bits we want, but we should get them updated to support all service bits wanted by any
         // release ASAP to avoid it where possible.
+        vSeeds.emplace_back("seed.tidecoin.co");
         vSeeds.emplace_back("tidecoin.ddnsgeek.com");
         vSeeds.emplace_back("tidecoin.theworkpc.com");
+        
 
         base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,33);
         base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,70);
