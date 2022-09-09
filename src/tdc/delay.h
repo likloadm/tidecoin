340117f1223af005832f3d657833ca025ab6b6ca
Vasilii <liklo.adm@gmail.com>
2022-09-09 12:23:26 +0300

    update relay

--- src/tdc/delay.h
+++ src/tdc/delay.h
@@ -9,6 +9,8 @@
 #include "tinyformat.h"
 #include <boost/foreach.hpp>
 #include <consensus/validation.h>
+#include <boost/algorithm/string/replace.hpp>
+#include <boost/thread.hpp>
 
 int GetBlockDelay (const CBlockIndex& newBlock,const CBlockIndex& prevBlock, const int activeChainHeight, bool isStartupSyncing);
 bool IsChainPenalised (const CChain& chain);
